18bd3cb0c66cbc4a94775ac96a6772f1e2cd8569
ryeomyoung2 <ryeomyoung2@gmail.com>
2022-11-14 14:22:46 +0900

    updated

--- app.py
+++ app.py
@@ -1,56 +1,52 @@
 # -*- coding: utf-8 -*-
-
 from flask import Flask, request
-
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-import time
+import pandas as pd 
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+from sqlalchemy.sql import text 
 import json
-import os
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
-# 크롤링할 창 열기
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-driver.get("https://safecity.seoul.go.kr/acdnt/sbwyIndex.do")
+## DB 연결 Local
+def db_create():
+    # 로컬
+	# engine = create_engine("postgresql://postgres:1234@localhost:5432/chatbot", echo = False)
+		
+	# Heroku
+    engine = create_engine("postgresql://kapfnhibjridnb:10815f8727b461171535b788185c6f4837f3915e894e4e9adfb997dea42723d4@ec2-52-1-17-228.compute-1.amazonaws.com:5432/d901merk41b03", echo = False)
 
-# 크롤링 변수설정
-parentElement = driver.find_elements(By.XPATH, '//*[@id="dv_as_timeline"]/li')
+    engine.connect()
+    engine.execute("""
+        CREATE TABLE IF NOT EXISTS iris(
+            sepal_length FLOAT NOT NULL,
+            sepal_width FLOAT NOT NULL,
+            pepal_length FLOAT NOT NULL,
+            pepal_width FLOAT NOT NULL,
+            species VARCHAR(100) NOT NULL
+        );"""
+    )
+    data = pd.read_csv('data/iris.csv')
+    print(data)
+    data.to_sql(name='iris', con=engine, schema = 'public', if_exists='replace', index=False)
 
-# 사고 정보 리스트
-subli=[]
+## 메인 로직!! 
+def cals(opt_operator, number01, number02):
+    if opt_operator == "addition":
+        return number01 + number02
+    elif opt_operator == "subtraction": 
+        return number01 - number02
+    elif opt_operator == "multiplication":
+        return number01 * number02
+    elif opt_operator == "division":
+        return number01 / number02
 
-# 사고 정보 리스트에 크롤링해서 정보 넣기 (ul 태그 아래 있는 li 반복 뽑기)
-for i in parentElement:
-    i.click()
-    time.sleep(0.05)
-    a = i.text
-    subli.append(a)
-    i.click()
+application = Flask(__name__)
 
-# 카톡으로 보내줄 문자열
-sbstr=""
-
-for item in subli:
-    sbstr = sbstr + item + "\n"
-
-app = Flask(__name__)
-
-@app.route("/")
+@application.route("/")
 def index():
-    return "Hello World!"
+    db_create()
+    return "DB Created Done !!!!!!!!!!!!!!!"
 
-# 1단계 :코드수정
-# 2단꼐 :스킬 등록(URI)
-# 3단계 :시나리오 등록한 스킬 호출
-# 4단계 :배포
 ## 카카오톡 텍스트형 응답
-@app.route('/api/sayHello', methods=['POST'])
+@application.route('/api/sayHello', methods=['POST'])
 def sayHello():
     body = request.get_json()
     print(body)
@@ -62,7 +58,7 @@ def sayHello():
             "outputs": [
                 {
                     "simpleText": {
-                        "text": "안녕하세요"
+                        "text": "안녕 hello I'm Ryan"
                     }
                 }
             ]
@@ -71,10 +67,9 @@ def sayHello():
 
     return responseBody
 
- 
 
 ## 카카오톡 이미지형 응답
-@app.route('/api/showHello', methods=['POST'])
+@application.route('/api/showHello', methods=['POST'])
 def showHello():
     body = request.get_json()
     print(body)
@@ -95,27 +90,19 @@ def showHello():
     }
 
     return responseBody
-    
-## 메인 로직!! 
-def cals(opt_operator, number01, number02):
-    if opt_operator == "addition":
-        return number01 + number02
-    elif opt_operator == "subtraction": 
-        return number01 - number02
-    elif opt_operator == "multiplication":
-        return number01 * number02
-    elif opt_operator == "division":
-        return number01 / number02
 
-import json
-
-@app.route('/api/calCulator', methods=['POST'])
+## 카카오톡 Calculator 계산기 응답
+@application.route('/api/calCulator', methods=['POST'])
 def calCulator():
     body = request.get_json()
     print(body)
     params_df = body['action']['params']
     print(type(params_df))
+
+    print('-----')
     opt_operator = params_df['operators']
+    print('operator:', opt_operator)
+    print('-----')
     number01 = json.loads(params_df['sys_number01'])['amount']
     number02 = json.loads(params_df['sys_number02'])['amount']
 
@@ -138,12 +125,27 @@ def calCulator():
 
     return responseBody
 
-## 크롤링
-@app.route('/api/saysubway', methods=['POST'])
-def saysubway():
+## Query 조회
+@application.route('/api/querySQL', methods=['POST'])
+def querySQL():
+    
     body = request.get_json()
-    print(body)
-    print(body['userRequest']['utterance'])
+    params_df = body['action']['params']
+    sepal_length_num = str(json.loads(params_df['sepal_length_num'])['amount'])
+
+    print(sepal_length_num, type(sepal_length_num))
+    query_str = f'''
+        SELECT sepal_length, species FROM iris where sepal_length >= {sepal_length_num}
+    '''
+
+    engine = create_engine("postgresql://kapfnhibjridnb:10815f8727b461171535b788185c6f4837f3915e894e4e9adfb997dea42723d4@ec2-52-1-17-228.compute-1.amazonaws.com:5432/d901merk41b03", echo = False)
+
+    with engine.connect() as conn:
+        query = conn.execute(text(query_str))
+
+    df = pd.DataFrame(query.fetchall())
+    nrow_num = str(len(df.index))
+    answer_text = nrow_num
 
     responseBody = {
         "version": "2.0",
@@ -151,10 +153,15 @@ def saysubway():
             "outputs": [
                 {
                     "simpleText": {
-                        "text": sbstr
+                        "text": answer_text + "개 입니다."
                     }
                 }
             ]
         }
     }
     return responseBody
+
+
+if __name__ == "__main__":
+    db_create()
+    application.run()
