12d523886cf5c57f678d73fb0a41d5bee554d8b3
ryeomyoung2 <ryeomyoung2@gmail.com>
2022-11-22 14:17:35 +0900

    updated

--- app.py
+++ app.py
@@ -74,20 +74,20 @@ def newslist():
     cur = conn.cursor()
 
     newhosun = '%' + hosun + '%'
-    cur.execute(f"SELECT title, link from news2 WHERE title LIKE '{newhosun}' order by id desc limit 3")
-    # cur.execute(f"SELECT * from newsdata WHERE newsname LIKE '{newhosun}' order by newsdate desc limit 5")
+    # cur.execute(f"SELECT title, link from news2 WHERE title LIKE '{newhosun}' order by id desc limit 3")
+    cur.execute(f"SELECT * from newsdata WHERE newsname LIKE '{newhosun}' order by newsdate desc limit 5")
     result_all = cur.fetchall()
     newsstr=""
 
-    # for i in result_all:
-    #     for j in i:
-    #         newsstr = newsstr + j + '\n'
-    #     newsstr = newsstr + '\n'
-    # newsstr = newsstr[:-2]
-
     for i in result_all:
         for j in i:
-            newsstr = newsstr + j + "\n"
+            newsstr = newsstr + j + '\n'
+        newsstr = newsstr + '\n'
+    newsstr = newsstr[:-2]
+
+    # for i in result_all:
+    #     for j in i:
+    #         newsstr = newsstr + j + "\n"
 
     responseBody = {
         "version": "2.0",
